d8a8b534f9d9aeb8ab553377fbb3a50af0bdb3ae
Abdulsathar27 <abdulsatharnechithodi@gmail.com>
2026-05-17 14:32:32 +0530

    create razorpay services

---
+++ lib/services/razorpay_service.dart
@@ -0,0 +1,50 @@
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+class RazorpayService {
+
+  late Razorpay razorpay;
+
+  void initialize({
+    required Function(PaymentSuccessResponse) onSuccess,
+    required Function(PaymentFailureResponse) onError,
+    required Function(ExternalWalletResponse) onWallet,
+  }) {
+
+    razorpay = Razorpay();
+
+    razorpay.on(
+      Razorpay.EVENT_PAYMENT_SUCCESS,
+      onSuccess,
+    );
+
+    razorpay.on(
+      Razorpay.EVENT_PAYMENT_ERROR,
+      onError,
+    );
+
+    razorpay.on(
+      Razorpay.EVENT_EXTERNAL_WALLET,
+      onWallet,
+    );
+  }
+
+  void openPayment() {
+
+    var options = {
+      'key': 'rzp_test_xxxxxxxxx',
+      'amount': 50000,
+      'name': 'Ticket Booking',
+      'description': 'Movie Ticket',
+      'prefill': {
+        'contact': '9876543210',
+        'email': 'test@gmail.com',
+      }
+    };
+
+    razorpay.open(options);
+  }
+
+  void dispose() {
+    razorpay.clear();
+  }
+}
