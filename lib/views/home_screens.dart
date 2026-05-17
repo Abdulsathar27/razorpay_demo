8c2d1708800074f5a1be2959a6b33846df4c450d
Abdulsathar27 <abdulsatharnechithodi@gmail.com>
2026-05-17 14:47:43 +0530

    create homescreen

---
+++ lib/views/home_screens.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:razorpaydemo/models/movie_model.dart';
+
+
+
+class HomeScreen extends StatelessWidget {
+
+  HomeScreen({super.key});
+
+  final MovieModel movie = MovieModel(
+    name: "Avengers",
+    image: "assets/images/movie.jpg",
+    price: 500,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Movies"),
+      ),
+
+      body: Center(
+        // child: MovieCard(movie: movie),
+      ),
+    );
+  }
+}
