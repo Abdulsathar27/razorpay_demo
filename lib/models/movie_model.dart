087498d5c8dd0764f78ab19ba2849aa347bfacb6
Abdulsathar27 <abdulsatharnechithodi@gmail.com>
2026-05-17 14:05:52 +0530

    create the models files

---
+++ lib/models/movie_model.dart
@@ -0,0 +1,11 @@
+class MovieModel {
+  final String name;
+  final String image;
+  final int price;
+
+  MovieModel({
+    required this.name,
+    required this.image,
+    required this.price,
+  });
+}
